e8c64eff81adae2e3430fd38f97cebcc92ee935c
ShivaBsnt <bbang2358@gmail.com>
2019-09-12 18:15:57 +0545

    [ADDcrud operation file addtion]

---
+++ aspdotnet crud/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using System.Linq;
+using System.Web;
+
+namespace New.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Release date is required.")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "Genre is required.")]
+        public string Genre { get; set; }
+        [Required(ErrorMessage = "price date is required.")]
+        public decimal Price { get; set; }
+    }
+
+
+  
+}
---
+++ aspdotnet crud/README.md
@@ -0,0 +1,1 @@
+# CRUD_Operation_With_ASPDOTNET-MVC-
